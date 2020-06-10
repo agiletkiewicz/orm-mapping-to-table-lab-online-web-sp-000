c38cf3b4ff67b8c6e2cd5a3d55589f0753b8c74c
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-06-10 21:38:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,7 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]   
   end
   
-  def self.create(attributes) 
+  def self.create(name:, grade:) 
     student = self.new(name, grade)
     student.save 
     student
